d197b585d766c544121698c365a4489aefa899a3
Andrey Gebrich <an_gebrich@outlook.com>
2021-10-21 18:22:12 +0300

    fix

--- autoload.php
+++ autoload.php
@@ -1,10 +1,4 @@
 <?php
 spl_autoload_register(function ($class) {
-    if(file_exists('./lib/' . $class . '.php')) {
         require_once './lib/' . $class . '.php';
-    } 
-    
-    else if (file_exists('./generators/' . $class . '.php')) {
-        require_once './generators/' . $class . '.php';
-    }
 });
