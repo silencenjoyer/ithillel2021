c7022002ccfaccdf1f898e936b64f43a5f687dd1
Andrey Gebrich <an_gebrich@outlook.com>
2021-11-21 10:39:13 +0200

    fix

--- patterns/Birdge/SenderGet.php
+++ patterns/Birdge/SenderGet.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 class SenderGet extends AbstractSender
 {
     public function send(int $phone, string $text): void
-    {var_dump($this->provider->getUrl(), $this->provider->getToken(), $phone, $text);die();
+    {
         $query = sprintf(
             '%s/send.php?token=%s&phones=%s&text=%s',
             $this->provider->getUrl(),
